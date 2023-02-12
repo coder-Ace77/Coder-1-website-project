d6c4125eeac55997e347b8d0ab32bb5793a324f7
A <mohdadil@iitbhilai.ac.in>
2023-02-12 16:01:49 +0530

    Previous Progress

---
+++ child-process/l.cpp
@@ -0,0 +1,26 @@
+//__gcd(a,b),abs(x),log2(x),log10(),log(),pow(x,y)
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<unordered_set>
+#include<algorithm>
+#include<string>
+#include<map>
+#define f(n) for(int i=0;i<n;i++)
+#define fr(i,n) for(int i=0;i<n;i+=1)
+#define ceil(a,b) (a+b-1)/b
+#define l long long int
+#define vi vector<int>
+#define pb(x) push_back(x)
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+    f(n){
+        cout<<i<<" ";
+    }    
+return 0;
+}
+//------------------------------------------------------------------------------------
