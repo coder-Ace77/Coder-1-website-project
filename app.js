38d6fcc788c7d6c71d3b37facccfd5ae9088c9f7
A <mohdadil@iitbhilai.ac.in>
2023-02-14 22:16:59 +0530

    Added contest form

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const mongoose = require('mongoose');
 const session  = require('express-session');
 const mongosession  = require('connect-mongodb-session')(session);
 const Schema = mongoose.Schema;
-const User = new Schema({
-    user:String,
-    password:String
-});
 
 const Ques = new Schema({
     ques_name:String,
@@ -23,15 +19,43 @@ const Ques = new Schema({
     sample_output:String,
     input_test:String,
     output_test:String,
-    difficulty:String
+    difficulty:String,
+    type:String,
+    author:String
 });
+
+const User = new Schema({
+    user:String,
+    password:String,
+    quesdone:Array,
+    quesmade:Array
+});
+
+const Contest = new Schema({
+    ID1:String,
+    ID2:String,
+    q1:String,
+    q2:String,
+    q3:String
+})
 
 const user = mongoose.model('user',User);
 const ques = mongoose.model('ques',Ques);
+const contest = mongoose.model('contest',Contest);
 
+app.set('view-engine','pug');
 app.use(body1.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'Public')));
-app.set('view-engine','pug');
+app.use(session({secret:"Key" ,resave:false,saveUninitialized:false}));
+
+app.post('/new_contest',(req,res)=>{
+    const val  = req.body;
+    const new_contest = new contest(val);
+    new_contest.save().then(result =>{
+        res.redirect('/');
+    })
+})
+
 // app.post('/sub/:ques',(req,res)=>{
 //     const ques_name= req.params.ques;
 //     console.log("ques:",ques_name);
@@ -70,26 +94,36 @@ app.set('view-engine','pug');
 
 app.post('/ques',(req,res)=>{
     console.log(req.body);
-    const new_ques = new ques(req.body);
+    const content = req.body;
+    content.author = req.session.user.user;
+    const new_ques = new ques(content);
             new_ques.save().then((result)=>{
-                res.redirect('/');
+                user.findOne({user:req.session.user.user}).then(val =>{
+                    if(val.quesmade==null){
+                        val.quesmade = [req.body.ques_name];
+                    }
+                    else{
+                        val.quesmade.push(req.body.ques_name);
+                    }
+                    val.save().then(result1=>{
+                        res.redirect('/');
+                    });
+                });
             });
 });
+
 app.use('/practice',(req,res)=>{
     ques.find().then(result=>{
         res.render('practice.pug',{arr:result});
     })
 });
 
-app.get('/:ques',(req,res)=>{
-    const ques_name = req.params.ques;
-    console.log(`id:${ques_name}`);
-    if(ques_name!='favicon.ico'){
-        ques.find({ques_name:ques_name}).then((result)=>{
-            console.log(result[0]);
-            const ob = result[0];
-            res.render('index.pug',ob);
-    });
+
+app.use('/sign',(req,res)=>{
+    if(req.session.isloggedin)
+        res.render('sign.pug',{user:req.session.user});
+    else{
+        res.render('sign.pug',{user:false});
     }
 });
 
@@ -97,7 +131,7 @@ app.use('/sign_in',(req,res)=>{
     user.find({user:req.body.ID}).then(result=>{
         if(result[0]){
             console.log('found');
-            res.render('sign.pug',{message:'*User already exist',val:false});
+            res.render('sign.pug',{message:'*User already exist',val:false,user:req.session.user});
         }
         else{
             bcrypt.hash(req.body.password,12).then((hashed)=>{
@@ -110,12 +144,51 @@ app.use('/sign_in',(req,res)=>{
     });
 });
 
-app.use('/sign',(req,res)=>{
-    res.render('sign.pug',{});
-});
+app.use('/login',(req,res)=>{
+    const id = req.body.ID;
+    const pass = req.body.password;
+    user.findOne({user:req.body.ID}).then(result =>{
+        if(!result){
+            res.redirect('/');
+        }
+        else{
+            bcrypt.compare(pass,result.password).then(value =>{
+                if(value==true){
+                    req.session.isloggedin = true;
+                    req.session.user = result;
+                    req.session.save(err =>{
+                        console.log("Logged");
+                        res.redirect('/');
+                    })
 
+                }
+                else{
+                    res.redirect('/')
+                }
+            })
+        }
+    })
+    
+});
+app.get('/:ques',(req,res)=>{
+    const ques_name = req.params.ques;
+    console.log(`id:${ques_name}`);
+    if(ques_name!='favicon.ico'){
+        ques.find({ques_name:ques_name}).then((result)=>{
+            console.log(result[0]);
+            const ob = result[0];
+            res.render('index.pug',ob);
+    });
+    }
+});
 app.use('/',(req,res)=>{
-    res.render('main.pug',{});
+    if(req.session.isloggedin){
+        console.log("Add:",req.session.user.quesmade);
+        res.render('main.pug',{user:req.session.user});
+    }
+    else{
+        res.render('main.pug',{user:{user:null}});
+    }
 });
 
 mongoose.connect('mongodb+srv://Mohd_Adil:Mishrapur@onlineide.5fsk0pr.mongodb.net/ide').then(result=>{
